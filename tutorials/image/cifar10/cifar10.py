0b60fbbf2d5b0a2e6ec09153cbef0b8d5fd26efb
HERO-Kwon <herokwon@gmail.com>
2018-05-25 00:06:06 +0900

    changed lr to 0.001 and no lr decay

--- tutorials/image/cifar10/cifar10.py
+++ tutorials/image/cifar10/cifar10.py
@@ -64,9 +64,9 @@ NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
 
 # Constants describing the training process.
 MOVING_AVERAGE_DECAY = 0.9999     # The decay to use for the moving average.
-NUM_EPOCHS_PER_DECAY = 350.0      # Epochs after which learning rate decays.
-LEARNING_RATE_DECAY_FACTOR = 0.1  # Learning rate decay factor.
-INITIAL_LEARNING_RATE = 0.01       # Initial learning rate.
+#NUM_EPOCHS_PER_DECAY = 350.0      # Epochs after which learning rate decays.
+#LEARNING_RATE_DECAY_FACTOR = 0.1  # Learning rate decay factor.
+INITIAL_LEARNING_RATE = 0.001       # Initial learning rate.
 
 # If a model is trained with multiple GPUs, prefix all Op names with tower_name
 # to differentiate the operations. Note that this prefix is removed from the
@@ -349,12 +349,14 @@ def train(total_loss, global_step):
   decay_steps = int(num_batches_per_epoch * NUM_EPOCHS_PER_DECAY)
 
   # Decay the learning rate exponentially based on the number of steps.
+  '''
   lr = tf.train.exponential_decay(INITIAL_LEARNING_RATE,
                                   global_step,
                                   decay_steps,
                                   LEARNING_RATE_DECAY_FACTOR,
                                   staircase=True)
-  tf.summary.scalar('learning_rate', lr)
+  '''
+  tf.summary.scalar('learning_rate', INITIAL_LEARNING_RATE)#lr)
 
   # Generate moving averages of all losses and associated summaries.
   loss_averages_op = _add_loss_summaries(total_loss)
