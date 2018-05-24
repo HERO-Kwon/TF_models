55a44ed027b7e620e9397e0a4441712c1668f1a4
HERO-Kwon <herokwon@gmail.com>
2018-05-24 19:02:13 +0900

    debug inf nan error

--- tutorials/image/cifar10/cifar10_input.py
+++ tutorials/image/cifar10/cifar10_input.py
@@ -234,7 +234,7 @@ def inputs(eval_data, data_dir, batch_size):
     # Read examples from files in the filename queue.
     read_input = read_cifar10(filename_queue)
     reshaped_image = tf.cast(read_input.uint8image, tf.float32)
-
+    '''
     height = IMAGE_SIZE
     width = IMAGE_SIZE
 
@@ -249,14 +249,15 @@ def inputs(eval_data, data_dir, batch_size):
     # Set the shapes of tensors.
     float_image.set_shape([height, width, 3])
     #resized_image.set_shape([height, width, 3])
+    '''
     read_input.label.set_shape([1])
-
+    
     # Ensure that the random shuffling has good mixing properties.
     min_fraction_of_examples_in_queue = 0.4
     min_queue_examples = int(num_examples_per_epoch *
                              min_fraction_of_examples_in_queue)
 
   # Generate a batch of images and labels by building up a queue of examples.
-  return _generate_image_and_label_batch(float_image, read_input.label,
+  return _generate_image_and_label_batch(reshaped_image, read_input.label,
                                          min_queue_examples, batch_size,
                                          shuffle=True)
